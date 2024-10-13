cc22546c393e38855e7939dd3cd08ab8c4094cda
y4-aoki <42287518+y4-aoki@users.noreply.github.com>
2024-10-13 13:52:13 +0000

    m

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ now=$(date +%Y%m%d-%H%M%S)
 branch=${1-master}
 
 update="cd /home/isucon/private_isu && git remote update && git checkout $branch && git pull"
-restart="cd /home/isucon/private_isu/webapp/golang && /user/local/go/bin/go build -o app && sudo systemctl restart isu-go"
+restart="cd /home/isucon/private_isu/webapp/golang && /usr/local/go/bin/go build -o app && sudo systemctl restart isu-go"
 rotate_mysql="sudo mv -v /var/log/mysql/slow.log /var/log/mysql/slow.log.$now && mysqladmin -uisuconp -pisuconp flush-logs"
 rotate_nginx="sudo mv -v /var/log/nginx/access.log /var/log/nginx/access.log.$now && sudo systemctl reload nginx"
 
